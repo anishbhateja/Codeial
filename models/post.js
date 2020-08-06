60658b1fc499d07a66654537237f9cc3a962c667
Anish Bhateja <anishbhateja99@gmail.com>
2020-08-06 12:05:59 +0530

    Database Relations-Postsand Comments added

--- models/post.js
+++ models/post.js
@@ -10,6 +10,13 @@ const postSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId, //post being created is linked to a user, this post will refer to that user
       ref: "User", //post will refer to User schema
     },
+    //include the array of ids of all comments in the post schema itself
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
   {
     timestamps: true,
