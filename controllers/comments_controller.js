8b810b761b6717642c60f8d208359f72cdbb1a5f
Anish Bhateja <anishbhateja99@gmail.com>
2020-08-06 16:35:12 +0530

    adding deleting comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,21 @@ module.exports.create = function (req, res) {
     }
   });
 };
+
+module.exports.destroy = function (req, res) {
+  Comment.findById(req.params.id, function (err, comment) {
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+      comment.remove();
+      Post.findByIdAndUpdate(
+        postId,
+        { $pull: { comments: req.params.id } },
+        function (post, err) {
+          return res.redirect("back");
+        }
+      ); //pulling the id of comment tobedeleted from comments in Post schema
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,10 @@ const passport = require("passport");
 const postController = require("../controllers/posts_controller");
 
 router.post("/create", passport.checkAuthentication, postController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postController.destroy
+); //see param method
 
 module.exports = router;
